937aa8781ac8364537b17ebefc72664c4e6a0c10
unknown <mdrakibulhoque83@gmail.com>
2021-09-02 18:43:58 +0600

    modify

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,16 +1,15 @@
 document.getElementById('error-message').style.display = 'none';
 document.getElementById('spinner').style.display = 'none';
-document.getElementById('team-details').textContent = ''
+
+// SearchBook Function
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-
     // clear data
     searchField.value = '';
 
     // Handle empty search request
-    if (searchText == '') {
-        // please write something to display
+    if (searchText === '') {
         displayError();
     }
     else {
@@ -18,47 +17,48 @@ const searchBook = () => {
         document.getElementById('spinner').style.display = 'block';
         // Hide error
         document.getElementById('error-message').style.display = 'none';
-        // Clear Team Details
-        document.getElementById('team-details').textContent = '';
+        // Clear Book Details
+        document.getElementById('book-details').textContent = '';
         // Clear Search Result
         document.getElementById('search-result').textContent = '';
         // load data
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-        console.log(url);
+        //console.log(url);
         fetch(url)
             .then(res => res.json())
             .then(data => displaySearchResult(data));
     }
 }
 
+// Error Function
 const displayError = () => {
     document.getElementById('error-message').style.display = 'block';
     document.getElementById('spinner').style.display = 'none';
-    document.getElementById('team-numbers').textContent = '';
-    document.getElementById('team-details').textContent = '';
-
+    document.getElementById('book-numbers').textContent = '';
 }
 
 // Display Search Result
 const displaySearchResult = books => {
-    // console.log(teams);
-    document.getElementById('team-numbers').textContent = '';
+    document.getElementById('book-numbers').textContent = '';
     const searchResult = document.getElementById('search-result');
+    // clear data
     searchResult.textContent = '';
 
     const bookList = books.docs;
-    console.log(bookList);
-    if (bookList == null) {
+    //console.log(bookList);
+
+    //Check if it is null or not
+    if (bookList === null) {
         displayError();
     }
     else {
         document.getElementById('error-message').style.display = 'none';
         document.getElementById('spinner').style.display = 'none';
-        document.getElementById('team-numbers').innerText = `Books Found ${bookList.length}`;
+        document.getElementById('book-numbers').innerText = `Books Found ${bookList.length}`;
         // Retrieve each book and display in a card
         bookList.forEach(book => {
-            console.log(book.title);
+            //console.log(book.title);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
@@ -67,49 +67,19 @@ const displaySearchResult = books => {
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="w-50 h-50 mx-auto" alt="...">
                 <div class="card-body">
                     <h5 id="titleBook" class="card-title">Title: ${book.title}</h5>
-                    <p class="card-text">Author Name:${book.author_name}</p>
-                    <p class="card-text">Publisher: ${book.publisher}</p>
-                    <p class="card-text">Published: ${book.first_publish_year}</p>
+                    <p class="card-text">Author Name: ${book.author_name[0]}</p>
+                    <p class="card-text">ISBN No: ${book.isbn[0]}</p>
+                    <p class="card-text">Language: ${book.language}</p>
+                    <p class="card-text">Publisher: ${book.publisher[0]}</p>
+                    <p class="card-text">First Published: ${book.first_publish_year}</p>
                 </div>
-                <div class = "card-footer">
-                    <button class="btn btn-outline-dark" onclick="loadTeamDetail('${book.title}')">Load More <i class="fas fa-arrow-right"></i></button> 
-                </div>
+
             </div>
             `;
+            //Append info to the div
             searchResult.appendChild(div);
         });
+
     }
 
 }
-
-/* 
-// // Fetch team detail
-const loadTeamDetail = () => {
-    //const authorUrl = `https://openlibrary.org/authors/${author_key}.json`;
-    // fetch(`http://openlibrary.org/search.json?title=${}`)
-    //     .then(res => res.json())
-    //     .then(res => displayTeamDetail(res)); 
-      const bookTitle =  document.getElementById('titleBook');
-      const bookTitleText = bookTitle.innerText;
-      console.log(bookTitleText);
-
-}
-// // Display team detail at the top
-const displayTeamDetail = (teamDetail) => {
-    const book = teamDetail.docs[0];
-    window.scrollTo(0, 40);
-    const teamShow = document.getElementById('team-details');
-    teamShow.textContent = ''
-    const div = document.createElement('div');
-    div.classList.add('card', 'bg-dark', 'text-warning', 'text-center');
-    div.innerHTML = `
-    <img src="${book.strStadiumThumb}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Team: ${book.alternate_names}</h5>
-        <p class="card-text">Home: ${book.strStadium}</p> 
-        <p><small class="card-text">Team Description: ${book.strDescriptionEN ? book.strDescriptionEN.slice(0, 100) : "N/a"}</small></p>
-        <a href="https://${book.strYoutube}" target="_blank" class="btn btn-primary">Watch Videos</a>
-    </div>
-    `;
-    teamShow.appendChild(div);
-} */
